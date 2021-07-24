5cc16ed4e1b08f3f12390c0f4230e07aba766780
Julie Rotter <mjrotter@msn.com>
2021-07-24 09:34:30 -0600

    Sat updates

--- Employee_Database_Challenge1.sql
+++ Employee_Database_Challenge1.sql
@@ -1,3 +1,6 @@
+-----------------------------------------------------------------------
+----DELIVERABLE #1
+-----------------------------------------------------------------------
 --***************************PART 1  a,b,c  
 --number of retiring employees by title MAKE RETIREMENT TITLES
 --use this to start   - FEEL confident about this work here -  new table retirement_titles - verify the join
@@ -48,4 +51,25 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
 --**********************************
+-----------------------------------------------------------------------------------------------
+----DELIVERABLE #2
+-----------------------------------------------------------------------------------------------
+---The employees eligible for mentorship program to train the next level - MAKE MENTORSHIP_ELIBILITY
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility 
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date  = '9999-01-01')  
+ORDER BY e.emp_no, to_date DESC;  
+
 
